224eb80c7239134bb2fe3cd2aa87181f44bed250
Sora <92116172+SoraNoTami@users.noreply.github.com>
2022-12-01 16:31:23 +0100

    Adding 4 new Controller (Action, Admin, Security, User)

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/')]
 class ContentController extends AbstractController
-{
-    #[Route('/content', name: 'app_content')]
-    public function index(): Response
+{   
+    //Main Page
+    #[Route('', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findByCreatedDate(3);
+
         return $this->render('content/index.html.twig', [
-            'controller_name' => 'ContentController',
+            'articles' => $articles,
         ]);
     }
 }
