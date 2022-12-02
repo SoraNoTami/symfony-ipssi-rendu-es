759a51e0691d005562e1d2d7f7430dc684b5e3bd
Sora <92116172+SoraNoTami@users.noreply.github.com>
2022-12-02 12:09:10 +0100

    Adding SQL and Correcting UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,11 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/profile/{id}', name: 'app_user')]
-    public function index(): Response
+    #[Route('/profile/{id}', name: 'app_user_show')]
+    public function getProfile(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
+        if ($user->getId() == $this->getUser()->getId()) {
+            $profile = $user;
+            $userArticles = $user->getArticles()->toArray();
+            $userProducts = $user->getProducts()->toArray();
+
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                $user->setPassword(
+                    $this->userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+    
+                $entityManager->persist($user);
+                $entityManager->flush();
+                
+                return $this->redirectToRoute('app_user_show', ["id" => $user->getId()]);
+            }
+                    
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $profile,
+            'articles' => $userArticles,
+            'products' => $userProducts,
+            'form' => $form->createView()
         ]);
+
+        } else {
+            return $this->redirectToRoute('app_home');
+        }
     }
 }
